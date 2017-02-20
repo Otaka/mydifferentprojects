2b38b7136e6be7cac9c6f65ebb195baf5463817f
otaka <demonmitnik@rambler.ru>
2017-02-20 21:51:10 +0200

    new test

---
+++ SimplePl/src/test/java/com/simplepl/grammar/StringParsingTest.java
@@ -0,0 +1,21 @@
+package com.simplepl.grammar;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class StringParsingTest extends BaseTest {
+
+    @Test
+    public void testGenericString() throws IOException {
+        MainParser parser = createParser();
+        checkTextRuleSuccess(parser, parser.testGenericStringRule(), "\"mystring\\nnewline\"");
+        checkTextRuleFailure(parser, parser.testGenericStringRule(), "\"mystring\\nnewline");
+    }
+    
+    @Test
+    public void testRawString() throws IOException {
+        MainParser parser = createParser();
+        checkTextRuleSuccess(parser, parser.testRawStringRule(), "\"\"\"mystring\"\"\"");
+        checkTextRuleFailure(parser, parser.testRawStringRule(), "\"\"\"mystringnewline");
+    }
+}
